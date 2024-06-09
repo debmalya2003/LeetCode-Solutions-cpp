99409e6a2af4bb3eb0c092547e4bfb6d7b992150
DebmalyaMukherjee <150362797+debmalya2003@users.noreply.github.com>
2024-06-09 12:16:07 +0530

    Fifteenth commit

--- June2024/15.cpp
+++ June2024/15.cpp
@@ -1,19 +1,28 @@
 //974. Subarray Sums Divisible by K
 
+#include <unordered_map>
+#include <vector>
+
 class Solution {
 public:
     int subarraysDivByK(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> map;
+        map[0] = 1;
+        int prefix_sum = 0;
         int count = 0;
         for (int i = 0; i < nums.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                sum += nums[j];
-                if (sum % k == 0) {
-                    count++;
-                }
+            prefix_sum += nums[i];
+            int remainder = prefix_sum % k;
+            if (remainder < 0) {
+                remainder += k;
+            }
+            if (map.find(remainder) != map.end()) {
+                count += map[remainder];
+                map[remainder]++;
+            } else {
+                map[remainder] = 1;
             }
         }
-        
         return count;
     }
 };
