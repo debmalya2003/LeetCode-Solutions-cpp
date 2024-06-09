490bbca5609231d223858051a220cc7355a48329
debmalya2003 <debmalyamukherjee84@gmail.com>
2024-06-10 00:22:17 +0530

    Updated 18th solution

--- June2024/18.cpp
+++ June2024/18.cpp
@@ -2,10 +2,21 @@
 
 class Solution {
 public:
-    bool isPowerOfThree(int n) 
-    {
-        if(n <= 0) return false;
-        double logValue = log(n) / log(3);
-        return logValue == (int)logValue;
+    bool isPowerOfThree(int n) {
+        double x = (double)n; 
+        while(true){
+            if(x == 1){
+                return true;
+            }
+            else if (x<=0){
+                return false;
+            }
+            x = x/3;
+        }
+        return false;
+
+
+
+        
     }
 };
