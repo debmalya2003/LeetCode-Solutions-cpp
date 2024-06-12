6bf7b58cfbc26e3566d92b4b66fc8878efaafb62
debmalya2003 <debmalyamukherjee84@gmail.com>
2024-06-12 19:13:11 +0530

    29th commit

---
+++ June2024/29.cpp
@@ -0,0 +1,24 @@
+//189.Rotate Array
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n; // Ensure k is within the range [0, n)
+
+        // Reverse the entire array
+        reverse(nums.begin(), nums.end());
+        
+        // Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        
+        // Reverse the rest of the elements after k
+        reverse(nums.begin() + k, nums.end());
+
+        //another approach could have been - 
+        //storing last k elements into temp vector
+        //shifting first n-k elements to the right
+        //copying from temp vector to nums vector
+    }
+};
