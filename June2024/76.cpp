cd3eb01a3fc055e7b91b0cf1c1c391d7d05d2aa4
debmalya2003 <debmalyamukherjee84@gmail.com>
2024-06-27 18:36:08 +0530

    76th commit

---
+++ June2024/76.cpp
@@ -0,0 +1,28 @@
+//1358. Number of Substrings Containing All Three Characters
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = size(s);
+
+        int i=0,j=0,count=0;
+
+        unordered_map<char, int> mp;
+
+        while(j<n)
+        {
+            mp[s[j]]++;
+
+            while(mp['a']>=1 && mp['b']>=1 && mp['c']>=1)
+            {
+                count += (n - j);
+
+                //shrinking the window
+                mp[s[i]]--;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+};
