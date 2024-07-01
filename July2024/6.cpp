3b061273cafb7d49f31754729641f7b49eec3c8b
debmalya2003 <debmalyamukherjee84@gmail.com>
2024-07-02 00:43:17 +0530

    6th commit

---
+++ July2024/6.cpp
@@ -0,0 +1,31 @@
+//56. Merge Intervals
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n=arr.size();
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < n; i++) { // select an interval:
+        int start = arr[i][0];
+        int end = arr[i][1];
+
+        //Skip all the merged intervals:
+        if (!ans.empty() && end <= ans.back()[1]) {
+            continue;
+        }
+
+        //check the rest of the intervals:
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j][0] <= end) {
+                end = max(end, arr[j][1]);
+            }
+            else {
+                break;
+            }
+        }
+        ans.push_back({start, end});
+    }
+    return ans;
+    }
+};
