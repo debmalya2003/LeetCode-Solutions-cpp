4c415f60a0baa20d8a8a2617f5e4e50ac506d074
debmalya2003 <debmalyamukherjee84@gmail.com>
2024-07-02 17:55:03 +0530

    13th commit

---
+++ July2024/13.cpp
@@ -0,0 +1,26 @@
+//205. Isomorphic Strings
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> indexS(200, 0); 
+        vector<int> indexT(200, 0); 
+        
+        int len = s.length(); 
+        
+        if(len != t.length()) { // If the lengths of the two strings are different, they can't be isomorphic
+            return false;
+        }
+        
+        for(int i = 0; i < len; i++) { 
+            if(indexS[s[i]] != indexT[t[i]]) { // Check if the index of the current character in string s is different from the index of the corresponding character in string t
+                return false; // If different, strings are not isomorphic
+            }
+            
+            indexS[s[i]] = i + 1; // updating position of current character
+            indexT[t[i]] = i + 1;
+        }
+        
+        return true; 
+    }
+};
