f5cbbdb100f2424d384aa53c6203d548c6d86d70
debmalya2003 <debmalyamukherjee84@gmail.com>
2024-07-10 23:57:18 +0530

    32nd commit

---
+++ July2024/32.cpp
@@ -0,0 +1,20 @@
+//1598. Crawler Log Folder
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        vector<string> paths_stack;
+
+        for (const string& log : logs) {
+            if (log == "../") {
+                if (!paths_stack.empty()) {
+                    paths_stack.pop_back();
+                }
+            } else if (log != "./") {
+                paths_stack.push_back(log);
+            }
+        }
+
+        return paths_stack.size();
+    }
+};
