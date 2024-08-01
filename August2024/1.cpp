8d656689c1d825f49fa6f712ab71dd8f0875476e
debmalya2003 <debmalyamukherjee84@gmail.com>
2024-08-01 15:59:42 +0530

    1st commit

---
+++ August2024/1.cpp
@@ -0,0 +1,13 @@
+//2678. Number of Senior Citizens
+
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        for (auto& x : details) {
+            int age = stoi(x.substr(11, 2));
+            ans += age > 60;
+        }
+        return ans;
+    }
+};
