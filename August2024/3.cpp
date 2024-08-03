e6eb8b9ae972c65461a94a00efb0ee06c0f77d0f
debmalya2003 <debmalyamukherjee84@gmail.com>
2024-08-03 10:49:16 +0530

    3rd commit

---
+++ August2024/3.cpp
@@ -0,0 +1,16 @@
+//1460. Make Two Arrays Equal by Reversing Subarrays
+
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        vector<int> cnt1(1001);
+        vector<int> cnt2(1001);
+        for (int& v : target) {
+            ++cnt1[v];
+        }
+        for (int& v : arr) {
+            ++cnt2[v];
+        }
+        return cnt1 == cnt2;
+    }
+};
